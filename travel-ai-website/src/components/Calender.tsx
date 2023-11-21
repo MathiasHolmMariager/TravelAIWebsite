2bfa8669eb8edd3609c32bd35af1687341bbe83a
Dankmod420 <thormodx@gmail.com>
2023-11-21 11:55:51 +0100

    updated fixes
    - navbar animatin on stays on same page on refresh
    - shows the stored value date on refresh

--- travel-ai-website/src/components/Calender.tsx
+++ travel-ai-website/src/components/Calender.tsx
@@ -1,50 +1,56 @@
 import dayjs from "dayjs";
+import { useState } from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useState } from "react";
 
 function Calender() {
-  const currentDate = dayjs();
-  const nextDay = currentDate.add(1, 'day');
+  // Check local storage for existing values
+  const storedDatesString = localStorage.getItem('DATE_VALUE');
+  const storedDates = storedDatesString ? JSON.parse(storedDatesString) : null;
+  const defaultFrom = dayjs();
+  const defaultTo = defaultFrom.add(1, 'day');
 
   // State to store selected dates
   const [selectedDates, setSelectedDates] = useState({
-    from: currentDate,
-    to: nextDay,
+    from: storedDates ? dayjs(storedDates.from) : defaultFrom,
+    to: storedDates ? dayjs(storedDates.to) : defaultTo,
   });
 
   // Function to handle date selection
   const handleDateChange = (date: any, type: any) => {
-    setSelectedDates((prevDates) => ({
-      ...prevDates,
-      [type]: date,
-    }));
+    setSelectedDates((prevDates) => {
+      const updatedDates = {
+        ...prevDates,
+        [type]: date,
+      };
 
-    localStorage.setItem('DATE_VALUE', JSON.stringify(selectedDates));
+      localStorage.setItem('DATE_VALUE', JSON.stringify(updatedDates));
+
+      return updatedDates;
+    });
   };
-  
-  
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <DemoContainer components={['DatePicker']}>
-      <DatePicker
-        label="FROM"
-        format="M/D/YYYY"
-        slotProps={{ field: { shouldRespectLeadingZeros: true } }}
-        value={selectedDates.from}
-        onChange={(date) => handleDateChange(date, 'from')}
-      />
-      <DatePicker
-        label="TO"
-        format="M/D/YYYY"
-        slotProps={{ field: { shouldRespectLeadingZeros: true } }}
-        value={selectedDates.to}
-        onChange={(date) => handleDateChange(date, 'to')}
-      />
-    </DemoContainer>
-  </LocalizationProvider>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker
+          label="FROM"
+          format="M/D/YYYY"
+          slotProps={{ field: { shouldRespectLeadingZeros: true } }}
+          value={selectedDates.from}
+          onChange={(date) => handleDateChange(date, 'from')}
+        />
+        <DatePicker
+          label="TO"
+          format="M/D/YYYY"
+          slotProps={{ field: { shouldRespectLeadingZeros: true } }}
+          value={selectedDates.to}
+          onChange={(date) => handleDateChange(date, 'to')}
+        />
+      </DemoContainer>
+    </LocalizationProvider>
   );
 }
 
