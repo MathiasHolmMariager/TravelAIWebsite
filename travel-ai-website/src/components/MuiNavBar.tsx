cae426aa29ef08cc57f6178384e065c2a3d48031
Dankmod420 <thormodx@gmail.com>
2023-12-05 12:02:46 +0100

    updated navbar og url

--- travel-ai-website/src/components/MuiNavBar.tsx
+++ travel-ai-website/src/components/MuiNavBar.tsx
@@ -11,13 +11,13 @@ import { useState } from "react";
 
 const stepPaths = [
   "/",
-  "/Fly",
-  "/Hotel",
-  "/persons",
-  "/oplevelser",
+  "/flight",
+  "/accommodation",
+  "/interrest",
+  "/experiences",
 ];
 
-const stepNames = ["Home", "Fly", "Hotel", "Persons", "Oplevelser"];
+const stepNames = ["Home", "Flight", "Accommodation", "Interrest", "Experiences"];
 
 const STORAGE_KEY = "CURRENT_PAGE_STEP";
 
