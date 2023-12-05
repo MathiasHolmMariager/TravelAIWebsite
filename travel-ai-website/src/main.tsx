96ec603876bcd00d33e82a5f199c21f9fb6c1968
Dankmod420 <thormodx@gmail.com>
2023-12-05 14:57:28 +0100

    Update main.tsx

--- travel-ai-website/src/main.tsx
+++ travel-ai-website/src/main.tsx
@@ -7,7 +7,5 @@ import { router } from "./router/router.tsx"
 import { RouterProvider} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <RouterProvider router={router}/>   
-  </React.StrictMode>,
 )
