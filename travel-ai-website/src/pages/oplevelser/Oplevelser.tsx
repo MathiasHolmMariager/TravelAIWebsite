09f65e8923397d95b64164e9f6c77002cbda89e3
N1k50 <nikolaj81561606233@live.dk>
2023-12-07 11:05:43 +0100

    Prompt change :question:

--- travel-ai-website/src/pages/oplevelser/Oplevelser.tsx
+++ travel-ai-website/src/pages/oplevelser/Oplevelser.tsx
@@ -71,7 +71,7 @@ export default function ReturnInput() {
         const kids = localStorage.getItem("kids");
         const city = localStorage.getItem("city");
 
-        const fullPrompt = `We are ${adults} adults and ${kids} kids going on a trip to ${city}. ${interests} What are some things we could do in the city that fit our interests? I want you to give me the things to do as a numbered list and please just only give me the list with a small description for each thing to do.`;
+        const fullPrompt = `We are ${adults} adults and ${kids} kids going on a trip to ${city}. ${interests} What are some things we could do in the city that fit our interests?`;
 
         const { assistantReply, conversationHistory: updatedHistory } =
           await OpenAItest(fullPrompt);
